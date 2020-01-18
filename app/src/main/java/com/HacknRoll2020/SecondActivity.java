e792f7dd574cfd3d4cd39226cd28788a299d7959
Mavis <maviskwa99@gmail.com>
2020-01-18 22:01:35 +0800

    no message

--- app/src/main/java/com/HacknRoll2020/SecondActivity.java
+++ app/src/main/java/com/HacknRoll2020/SecondActivity.java
@@ -1,10 +1,13 @@
 package com.HacknRoll2020;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +16,7 @@ public class SecondActivity extends AppCompatActivity {
     Button saveButton, cancelButton;
     EditText toiletEditText;
     SeekBar seekBar;
+    MediaPlayer player;
 
 
     @Override
@@ -31,17 +35,32 @@ public class SecondActivity extends AppCompatActivity {
                 int stepsToToilet = Integer.parseInt(toiletEditText.getText().toString());
                 int progressLevel = seekBar.getProgress();
 
+                if(player == null) {
+                    if (progressLevel == 0) {
+                        player = (MediaPlayer) MediaPlayer.create(this, R.raw.light_sleeper);
+                    } else if (progressLevel == 1) {
+                        player = (MediaPlayer) MediaPlayer.create(this, R.raw.medium_sleeper);
+                    } else if (progressLevel == 2) {
+                        player = (MediaPlayer) MediaPlayer.create(this, R.raw.heavy_sleeper);
+                    }
+
+                    player.start();
+                }
+
+
 
             }
         });
 
-//        cancelButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(this, MainActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (player != null)
+                player.release();
+                player = null;
+                Toast.makeText(this, "Media stopped", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
