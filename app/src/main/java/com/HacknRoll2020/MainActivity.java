3e6b7158dea4089295ae0d3aa2dffb6ff9e20a47
Berttwm <BertrandTWM@gmail.com>
2020-01-18 17:15:38 +0800

    ok

--- app/src/main/java/com/HacknRoll2020/MainActivity.java
+++ app/src/main/java/com/HacknRoll2020/MainActivity.java
@@ -1,15 +1,35 @@
 package com.HacknRoll2020;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.addAlarm);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                openSecondActivity();
+            }
+        });
+    }
+
+    public void openSecondActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+
     }
 
 }
