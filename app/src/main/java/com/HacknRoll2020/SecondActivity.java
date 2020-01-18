7a464fbad807ffcc4dadba628a2e923169b7d65a
Berttwm <BertrandTWM@gmail.com>
2020-01-18 21:20:14 +0800

    ok

--- app/src/main/java/com/HacknRoll2020/SecondActivity.java
+++ app/src/main/java/com/HacknRoll2020/SecondActivity.java
@@ -1,6 +1,8 @@
 package com.HacknRoll2020;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,7 +10,17 @@ public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final EditText toiletEditText = (EditText) findViewById(R.id.toiletEditText);
+        Button saveButton = (Button) findViewById(R.id.saveButton);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+//        saveButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                int steps = Integer.parseInt(toiletEditText.getText().toString());
+//            }
+//        });
     }
 }
